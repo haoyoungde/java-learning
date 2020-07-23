1e0f7bc31e104285c911e044094312395ec160df
wuzhihao <wuzhihao@eptok.com>
2020-07-23 09:12:48 +0800

    rocksdb

--- rocksdb/src/main/java/com/haoyoungde/Main.java
+++ rocksdb/src/main/java/com/haoyoungde/Main.java
@@ -5,6 +5,9 @@ import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     private static final int _1MB = 1024 * 1024;
@@ -16,19 +19,30 @@ public class Main {
                 Options options = new Options().setCreateIfMissing(true);
                 RocksDB rocksdb = RocksDB.open(options, "db")
         ){
-            for(int i = 0; i < Integer.MAX_VALUE; i++){
-                byte[] key = new byte[1024];
-                byte[] value = new byte[1024];
-                rocksdb.put(key, value);
-                Main a = new Main();
-                Main b = new Main();
-                a.instance = b;
-                b.instance = a;
-                a=null;
-                b=null;
+            ExecutorService executorService = Executors.newFixedThreadPool(10);
+            executorService.execute(() -> {
+                for(int i = 0; i < 500000; i++){
+                    byte[] key = new byte[1024];
+                    byte[] value = new byte[1024];
+                    try {
+                        rocksdb.put(key, value);
+                    } catch (RocksDBException e) {
+                        e.printStackTrace();
+                    }
+                    Main a = new Main();
+                    Main b = new Main();
+                    a.instance = b;
+                    b.instance = a;
+                    a=null;
+                    b=null;
+                }
+            });
+            executorService.shutdown();
+            while (!executorService.awaitTermination(1, TimeUnit.MINUTES)){
+                System.out.println("等待任务结束");
             }
             System.in.read();
-        } catch (RocksDBException | IOException e) {
+        } catch (RocksDBException | IOException | InterruptedException e) {
                 e.printStackTrace();
         }
     }
